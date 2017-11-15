708a96cd53ad70f31bb9fd54635a318461eb685b
Mendel Agterberg <magterberg@mirabeau.nl>
2017-11-15 16:16:08 +0100

    Setup project and repository

---
+++ BSNChecker.ConsoleApp/BSNChecker.ConsoleApp.Tests/ValidateTests.cs
@@ -0,0 +1,14 @@
+﻿using Xunit;
+
+namespace BSNChecker.ConsoleApp.Tests
+{
+    public class ValidateTests
+    {
+        [Fact]
+        public void Validate_ShouldInitialize_WithValidParameters()
+        {
+            var validate = new Validate();
+            Assert.NotNull(validate);
+        }
+    }
+}
