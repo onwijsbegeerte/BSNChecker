c2720627d2f82f455df0d9d012b32b08ff002ffd
Mendel Agterberg <magterberg@mirabeau.nl>
2017-11-15 16:32:33 +0100

    Started working on regex implementation

--- BSNChecker.ConsoleApp/BSNChecker.ConsoleApp.Tests/ValidateTests.cs
+++ BSNChecker.ConsoleApp/BSNChecker.ConsoleApp.Tests/ValidateTests.cs
@@ -7,8 +7,29 @@ namespace BSNChecker.ConsoleApp.Tests
         [Fact]
         public void Validate_ShouldInitialize_WithValidParameters()
         {
-            var validate = new Validate();
-            Assert.NotNull(validate);
+            var validator = new Validator();
+            Assert.NotNull(validator);
+        }
+
+        [Fact]
+        public void Validate_ShouldReturnTrue_WithValidParameters()
+        {
+            var validator = new Validator();
+            Assert.True(validator.ValidateBsnNumber("000000000"));
+        }
+
+        [Fact]
+        public void Validate_ShouldReturnFalse_WithInvalidParameters()
+        {
+            var validator = new Validator();
+            Assert.False(validator.ValidateBsnNumber("0000"));
+        }
+
+        [Fact]
+        public void Validate_ShouldReturnFalse_InvalidParameters()
+        {
+            var validator = new Validator();
+            Assert.False(validator.ValidateBsnNumber("asdasdasd"));
         }
     }
 }
---
+++ BSNChecker.ConsoleApp/BSNChecker.ConsoleApp/Validator.cs
@@ -0,0 +1,20 @@
+﻿using System.Runtime.Remoting.Messaging;
+using System.Text.RegularExpressions;
+
+namespace BSNChecker.ConsoleApp
+{
+    public class Validator
+    {
+        public bool ValidateBsnNumber(string input)
+        {
+            if (input.Length != 8 || input.Length != 9)
+            {
+                return false;
+            }
+
+            Regex regex = new Regex(@"[0-9]");
+
+            return regex.IsMatch(input);
+        }
+    }
+}
