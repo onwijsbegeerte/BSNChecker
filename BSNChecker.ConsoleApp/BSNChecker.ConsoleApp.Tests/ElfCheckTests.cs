5c498a2151ab436d77f3fbd4561071f50db3fcd3
Mendel Agterberg <magterberg@mirabeau.nl>
2017-11-29 14:34:51 +0100

    Wrote failing test for method

--- BSNChecker.ConsoleApp/BSNChecker.ConsoleApp.Tests/ElfCheckTests.cs
+++ BSNChecker.ConsoleApp/BSNChecker.ConsoleApp.Tests/ElfCheckTests.cs
@@ -4,10 +4,21 @@ namespace BSNChecker.ConsoleApp.Tests
     public class ElfCheckTests
     {
         [Fact]
-        public void Validate_ShouldInitialize_WithValidParameters()
+        public void elfChecker_ShouldInitialize_WithValidParameters()
         {
             var elfChecker = new ElfChecker();
             Assert.NotNull(elfChecker);
+        }
+
+        [Fact]
+        public void elfCheckerCheck_ShouldReturnTrue_WithValidParameters()
+        {
+            //123456782
+            //111222333 .
+
+            var elfChecker = new ElfChecker();
+            var result = elfChecker.Check("123456782");
+            Assert.True(result);
         }
     }
 }
