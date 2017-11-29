4717a0162784edaed317b74c97b59f863f29c929
Mendel Agterberg <magterberg@mirabeau.nl>
2017-11-29 14:29:39 +0100

    Wrote test for elfchecker

---
+++ BSNChecker.ConsoleApp/BSNChecker.ConsoleApp.Tests/ElfCheckTests.cs
@@ -0,0 +1,13 @@
+﻿using Xunit;
+namespace BSNChecker.ConsoleApp.Tests
+{
+    public class ElfCheckTests
+    {
+        [Fact]
+        public void Validate_ShouldInitialize_WithValidParameters()
+        {
+            var elfChecker = new ElfChecker();
+            Assert.NotNull(elfChecker);
+        }
+    }
+}
